23448b767f5095019ba6bfa06e1c4eb624b93820
Malin Nilsson <malin_christina@yahoo.se>
2024-09-15 06:41:13 +0000

    add CSRF_TRUSTED_ORIGINS

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -41,6 +41,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://8000-malinchristina-drfapi-l5vvofrrqis.ws.codeinstitute-ide.net',
+]
+
 
 # Application definition
 
