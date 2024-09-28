9c9a94325821426e59cfde02c9609a442d0fba8e
Malin Nilsson <malin_christina@yahoo.se>
2024-09-28 11:37:29 +0000

    add dev frontend address

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -81,6 +81,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'https://8000-malinchristina-drfapi-l5vvofrrqis.ws.codeinstitute-ide.net',
+    'https://3000-malinchristin-wtmoments-p3do9mi96dd.ws.codeinstitute-ide.net/',
 ]
 
 if "CLIENT_ORIGIN" in os.environ:
